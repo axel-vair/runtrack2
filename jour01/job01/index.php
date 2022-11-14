e95632405abb36faf804e486d9bc7d8984b43a7e
axel-vair <axelvair@icloud.com>
2022-11-14 14:55:38 +0100

    add j1

---
+++ jour01/job01/index.php
@@ -0,0 +1,34 @@
+<?php 
+    #Create variables strings
+    $str = "LaPlateforme ";
+
+    $str2 = "Vive ";
+
+    $str3 = "!";
+
+    #Display str 
+    echo "Ma variable str = ". $str ."<br>";
+
+    #Display value of variables concatanate
+    echo "Ma concaténation = ". $str2 . $str . $str3 ."<br>";
+
+
+    #Create variable number
+    $val = 6;
+
+    #Display variable number
+    echo "Variable val = ". $val ."<br>";
+    
+
+    #Add 4 at val
+    echo "Val + 4 = " . $val + 4 ."<br>";
+
+    #Create variable boolean 
+    $myBool = true;
+
+    #Display variable myBool
+    echo "Mon boolean = ".$myBool;
+
+?>
+
+    
