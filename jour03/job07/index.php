30b9f5a88bf53c92839e69616666f64da8b23fb3
axel-vair <axelvair@icloud.com>
2022-11-16 15:24:52 +0100

    oops bad version before :x

--- jour03/job07/index.php
+++ jour03/job07/index.php
@@ -3,10 +3,13 @@ $str = "Certaines choses changent, et d'autres ne changeront jamais.";
 $str2 = "";
 
 #On boucle dans la string str
-#Si isset n'est pas
+#Si isset de str i +1 n'est pas vide
+#Alors str2 = l'index0 de str
+#Sinn str2 = l'index +1 de str
 for($i = 0; isset($str[$i]); $i++){
-    if (isset($str[$i+1])){
+    if (!isset($str[$i+1])){
         $str2 = $str2.$str[0];
+
     }else{ 
         $str2 = $str2.$str[$i+1];
     }
