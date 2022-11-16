711a1c7f1e489bad624c616681376e669fefaa5c
axel-vair <axelvair@icloud.com>
2022-11-16 12:28:54 +0100

    add charset that doesnt work

---
+++ jour03/job06/index.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$str = "Les choses que l'on possede finissent par nous posseder.";
+#J'enlève les accents :( le UTF-8 marche pô 
+#Boucle sort lambda mais au lieu d'incrémenter on décrémente pour aller à l'envers
+
+for($i = 0; isset($str[$i]); $i--){ 
+    echo $str[$i];
+}
+
+?>
+</body>
+</html>
