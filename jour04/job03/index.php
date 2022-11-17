5eee2f5aa8a6c99061ab7301fc108c551e36dc95
axel-vair <axelvair@icloud.com>
2022-11-17 10:49:19 +0100

    add job03

---
+++ jour04/job03/index.php
@@ -0,0 +1,24 @@
+<form method="post">  
+     Entrez votre Nom  <br>  
+    <input type="text" name="username">  
+    <br>  Entrez votre Prénom  <br>  
+    <input type="text" name="firstname"> 
+    <br>  Entrez votre Email  <br>  
+    <input type = "email" name = "email"> 
+    <br>
+    <br>
+    <input type="submit" value="envoyer">
+  </form>  
+
+  <?php
+
+#foreach du tableau $get qui fait un echo de $value
+#initiation de count, incrémentation de la variable et écho du nombre total
+$count = 0;
+foreach ($_POST as $key => $value){ 
+     $count++;
+
+
+}
+echo $count;
+  ?>
