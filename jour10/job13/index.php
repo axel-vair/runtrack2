7f0be0c86d2dac962189df925a2847283481dbac
axel-vair <axelvair@icloud.com>
2022-11-30 17:39:26 +0100

    forgot semilicon

--- jour10/job13/index.php
+++ jour10/job13/index.php
@@ -1,1 +1,46 @@
 <?php
+    $mysqli = new mysqli("localhost", "root", "root", "jour09");
+    $sql = "INSERT INTO utilisateurs(nom, prenom, login, password) VALUES ()";
+
+    $result = $mysqli -> query($conn, $sql);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tableau</title>
+</head>
+<style>
+    th, td {
+        border: 1px solid black;
+        padding: 1rem;
+        text-align: center;
+    }
+</style>
+<body>
+
+<table>
+    <tr>
+        <th>Nom des salles</th>
+        <th>Nom des étages</th>
+    </tr>
+    <?php
+    foreach ($result as $line) {
+        echo "<tr>";
+        foreach ($line as $case) {
+            echo "<td>" . $case . "</td>";
+        }
+        echo "</tr>";
+    }
+    ?>
+
+
+</table>
+</body>
+</html>
